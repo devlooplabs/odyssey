7ecc10412c3487758e931f313c98e12849be0f88
Mateus Nogueira <mateus.nogueira@delage.com.br>
2024-10-15 19:37:13 -0300

    wip

--- apps/front/src/components/content/card/content-card-group.tsx
+++ apps/front/src/components/content/card/content-card-group.tsx
@@ -3,11 +3,11 @@ import { ContentCard } from "./content-card";
 export function ContentCardGroup() {
   return (
     <div className="w-full flex gap-8 justify-center flex-wrap">
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
-      <ContentCard />
+      <ContentCard name="Teste" />
+      <ContentCard name="Teste" />
+      <ContentCard name="Teste" />
+      <ContentCard name="Teste" />
+      <ContentCard name="Teste" />
     </div>
   );
 }
